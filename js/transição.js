c59eb015bef33c7074e4157676a631bd3023005c
eduardoferreira97 <animedudu12345@gmail.com>
2023-05-18 22:07:22 -0300

    Adição aba skill

--- js/transição.js
+++ js/transição.js
@@ -3,7 +3,7 @@ window.onscroll = function () { scrollFunction() };
     function scrollFunction() {
         if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
 
-            document.getElementById("navbar").style.transition = "background-color 0.2s linear";
+            document.getElementById("navbar").style.transition = "background-color 0.5s linear";
             document.getElementById("navbar").style.background = "#4C35D2";
 
         } else {
